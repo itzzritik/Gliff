f1340984204f4d5a9d234da0566b840819cc70d0
Jarvis <ritik.jarvis@gmail.com>
2026-05-22 08:27:30 +0000

    publish v0.0.12

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,4 +1,4 @@
-const SHA = "743c1dea64ca2d3787ad1da89ec753a059342c5b";
+const SHA = "d3f089fe67783c95f26099f3ef962176f2895fd2";
 const BASE = `https://cdn.jsdelivr.net/gh/itzzjarvis/Assets@${SHA}/styles/fa`;
 
 export const ASSETS_FA_CSS = `${BASE}/fa.css`;
